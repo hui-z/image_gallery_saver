61053e3781bd131de87a604cf95f900293624d6b
heyongjian <hyjfine@gmail.com>
2020-06-22 14:43:37 +0800

    update pub dependencies

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,13 +1,12 @@
 import 'dart:typed_data';
+import 'dart:ui' as ui;
 
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:image_gallery_saver/image_gallery_saver.dart';
-
-import 'package:permission_handler/permission_handler.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:dio/dio.dart';
-import 'dart:ui' as ui;
+import 'package:permission_handler/permission_handler.dart';
 
 void main() => runApp(MyApp());
 
@@ -35,9 +34,12 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
-    PermissionHandler().requestPermissions(<PermissionGroup>[
-      PermissionGroup.storage, // 在这里添加需要的权限
-    ]);
+
+    _requestPermission();
+
+//    PermissionHandler().requestPermissions(<PermissionGroup>[
+//      PermissionGroup.storage, // 在这里添加需要的权限
+//    ]);
   }
 
   @override
@@ -89,26 +91,39 @@ class _MyHomePageState extends State<MyHomePage> {
         ));
   }
 
+  _requestPermission() async {
+    Map<Permission, PermissionStatus> statuses = await [
+      Permission.storage,
+    ].request();
+
+    print(statuses[Permission.storage]);
+  }
+
   _saveScreen() async {
     RenderRepaintBoundary boundary =
         _globalKey.currentContext.findRenderObject();
     ui.Image image = await boundary.toImage();
     ByteData byteData = await image.toByteData(format: ui.ImageByteFormat.png);
-    final result = await ImageGallerySaver.saveImage(byteData.buffer.asUint8List());
+    final result =
+        await ImageGallerySaver.saveImage(byteData.buffer.asUint8List());
     print(result);
   }
 
   _getHttp() async {
-    var response = await Dio().get("https://ss0.baidu.com/94o3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=a62e824376d98d1069d40a31113eb807/838ba61ea8d3fd1fc9c7b6853a4e251f94ca5f46.jpg", options: Options(responseType: ResponseType.bytes));
-    final result = await ImageGallerySaver.saveImage(Uint8List.fromList(response.data));
+    var response = await Dio().get(
+        "https://ss0.baidu.com/94o3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=a62e824376d98d1069d40a31113eb807/838ba61ea8d3fd1fc9c7b6853a4e251f94ca5f46.jpg",
+        options: Options(responseType: ResponseType.bytes));
+    final result =
+        await ImageGallerySaver.saveImage(Uint8List.fromList(response.data));
     print(result);
   }
 
- _saveVideo() async {
+  _saveVideo() async {
     var appDocDir = await getTemporaryDirectory();
     String savePath = appDocDir.path + "/temp.mp4";
-    await Dio().download("http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4", savePath);
+    await Dio().download(
+        "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4", savePath);
     final result = await ImageGallerySaver.saveFile(savePath);
     print(result);
- }
+  }
 }
